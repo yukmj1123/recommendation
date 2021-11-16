7054707795ad7eb8c7bc5242bb0fb4d55d1715d6
jeongminheo <gjwjdals56@gmail.com>
2021-11-16 11:44:36 +0900

    connect

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -8,5 +8,7 @@ from django.conf import settings
 urlpatterns = [
     #*path('', views.index, name='index'),
     path('', views.mainpage, name='mainpage'),
+    path('new/', views.new),
+    path('third/', views.third),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mainpage/views.py
+++ mainpage/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 def mainpage(request):
     return render(request, 'mainpage.html')
+
+def new(request):
+    return render(request, 'new.html')
+    
+def third(request):
+    return render(request, 'third.html')
     

86aa322e1ec37bdfb4dbf560bb7ca251430fcb0d
jeongminheo <gjwjdals56@gmail.com>
2021-12-10 20:57:30 +0900

    db데이터불러오기 test1

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -5,13 +5,11 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
     #*path('', views.index, name='index'),
     path('', views.mainpage, name='mainpage'),
     path('new/', views.new),
     path('third/', views.third),
-
-
+    path('list',views.listFunc)
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
